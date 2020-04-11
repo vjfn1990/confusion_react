bc7f12fe47ce954b4bb25628c3e036c3b4f8cf2f
Victor Felipe <vjfn1990@gmail.com>
2020-04-11 21:58:55 +0100

    RenderLeader functional component added

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -2,21 +2,27 @@ import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
+const RenderLeader = ({ leader }) => {
+    return(
+        <Media tag="li">
+            <Media left middle>
+                <Media object src={leader.image} alt={leader.name} />
+            </Media>
+            <Media body className="ml-5">
+                <Media heading>{leader.name}</Media>
+                <p>{leader.designation}</p>
+                <p>{leader.description}</p>
+            </Media>
+        </Media>
+    );
+}
+
 function About(props) {
     
     const leaders = props.leaders.map((leader) => {
         return (
             <div key={leader.id} className="col-12 mt-5">
-                <Media tag="li">
-                    <Media left middle>
-                        <Media object src={leader.image} alt={leader.name} />
-                    </Media>
-                    <Media body className="ml-5">
-                        <Media heading>{leader.name}</Media>
-                        <p>{leader.designation}</p>
-                        <p>{leader.description}</p>
-                    </Media>
-                </Media>
+                <RenderLeader leader={leader} />
             </div>
         );
     });
