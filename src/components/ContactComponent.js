0a6ee81466c4f81bd0b9288042779f125b5968fb
Victor Felipe <vjfn1990@gmail.com>
2020-04-26 21:38:29 +0100

    Warnings suppressed

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -45,7 +45,7 @@ class Contact extends Component {
                             HONG KONG<br />
                             <i className="fa fa-phone"></i>: +852 1234 5678<br />
                             <i className="fa fa-fax"></i>: +852 8765 4321<br />
-                            <i className="fa fa-envelope"></i>: <a href="mailto:confusion@food.net">confusion@food.net</a>
+                            <i className="fa fa-envelope"></i>: <a href="https://www.google.com">confusion@food.net</a>
                             </address>
                     </div>
                     <div className="col-12 col-sm-6 offset-sm-1">
@@ -53,9 +53,9 @@ class Contact extends Component {
                     </div>
                     <div className="col-12 col-sm-11 offset-sm-1">
                         <div className="btn-group" role="group">
-                            <a role="button" className="btn btn-primary" href="tel:+85212345678"><i className="fa fa-phone"></i> Call</a>
-                            <a role="button" className="btn btn-info"><i className="fa fa-skype"></i> Skype</a>
-                            <a role="button" className="btn btn-success" href="mailto:confusion@food.net"><i className="fa fa-envelope-o"></i> Email</a>
+                            <a role="button" className="btn btn-primary" href="https://www.google.com"><i className="fa fa-phone"></i> Call</a>
+                            <a role="button" className="btn btn-info" href="https://www.google.com"><i className="fa fa-skype"></i> Skype</a>
+                            <a role="button" className="btn btn-success" href="https://www.google.com"><i className="fa fa-envelope-o"></i> Email</a>
                         </div>
                     </div>
                 </div>
